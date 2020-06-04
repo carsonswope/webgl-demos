559e3f66c04084ce8ef7ef3ec0065b4337e65e0c
Carson Swope <carsonmswope@gmail.com>
2020-06-04 09:57:12 -0600

    now rendering 3d cube

--- src/main.ts
+++ src/main.ts
@@ -2,42 +2,94 @@
 
 import * as twgl from '../node_modules/twgl.js/dist/4.x/twgl-full'
 
+import { Cube } from './cube'
+
+// class ProgramData {
+  // public ProgramData(gl: WebGL2RenderingContext, p: WebGLProgram) {
+    // const num_attrs = gl.getProgramParameter(p, gl.ACTIVE_ATTRIBUTES)
+
+  // }
+
+
+// }
+
+// function 
+
 (() => {
 
-  const gl = document.querySelector('canvas').getContext('webgl2')
-  const programInfo = twgl.createProgramInfo(gl, ["vs1_vert", "fs1_frag"]);
+  const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+  const gl = canvas.getContext('webgl2')
+  if (!gl) { alert('WebGL2 not enabled for your browser!') }
 
-  const arrays = {
-     position: [
-     	-1, -1, 0,
-     	 1, -1, 0,
-     	-1,  1, 0,
-     	-1,  1, 0,
-     	 1, -1, 0,
-     	 1,  1, 0],
-  };
+  const programInfo = twgl.createProgramInfo(gl, ["vs1_vert", "fs1_frag"])
+  
+  // configure vertex array!
+  const vao = gl.createVertexArray()
+  gl.bindVertexArray(vao)
 
-  const bufferInfo = twgl.createBufferInfoFromArrays(gl, arrays);
+  const positionBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+  const posAttrLocation = gl.getAttribLocation(programInfo.program, "position")
+  gl.enableVertexAttribArray(posAttrLocation)
+  gl.vertexAttribPointer(posAttrLocation, 4, gl.FLOAT, false, 0, 0)
 
-  // twgl.createBufferFromArray
+  const colorBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+  const colAttrLocation = gl.getAttribLocation(programInfo.program, 'color');
+  gl.enableVertexAttribArray(colAttrLocation)
+  gl.vertexAttribPointer(colAttrLocation, 4, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null)
+
+  const indexBuffer = gl.createBuffer()
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+  gl.bindVertexArray(null)
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null)
+
+  // post data!
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer)
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, Cube.idxes(), gl.STATIC_DRAW)
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, null);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer)
+  gl.bufferData(gl.ARRAY_BUFFER, Cube.vtxes(), gl.STATIC_DRAW)
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer)
+  const colors = [
+     0,  1,  0, 1,
+     0,  0,  1, 1,
+     0,  1,  0, 1,
+     0,  0,  1, 1,
+     0,  1,  0, 1,
+     0,  0,  1, 1,
+     0,  1,  0, 1,
+     0,  0,  1, 1,
+  ]
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW)
+  gl.bindBuffer(gl.ARRAY_BUFFER, null)
+
+  gl.enable(gl.DEPTH_TEST)
 
   const render = (time) => {
 
-  	// debugger;
+    twgl.resizeCanvasToDisplaySize(canvas)
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
 
-  	twgl.resizeCanvasToDisplaySize(<HTMLCanvasElement>gl.canvas)
-
-  	gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-  	const uniforms = {
-  		time: time * 0.001,
-  		resolution: [gl.canvas.width, gl.canvas.height],
-  	}
+    const aspect = canvas.clientWidth / canvas.clientHeight;
+    const cam_proj = twgl.m4.perspective(60 * Math.PI / 180, aspect, 1, 2000)
+    const cam_pos = twgl.m4.translate(twgl.m4.identity(), twgl.v3.create(0.5, 0, -10))
+    const cam_pos_inv = twgl.m4.inverse(cam_pos)
+    const obj_pos = twgl.m4.axisRotate(twgl.m4.identity(), twgl.v3.create(0.707, 0.707, 0), time * 0.001)
 
   	gl.useProgram(programInfo.program)
-  	twgl.setBuffersAndAttributes(gl, programInfo, bufferInfo)
-  	twgl.setUniforms(programInfo, uniforms)
-  	twgl.drawBufferInfo(gl, bufferInfo)
+    gl.bindVertexArray(vao)
+  	twgl.setUniforms(programInfo, {
+      'cam_proj': cam_proj,
+      'cam_pos_inv': cam_pos,
+      'obj_pos': obj_pos,
+    })
+
+    gl.drawElements(gl.TRIANGLES, 36, gl.UNSIGNED_SHORT, 0)
 
   	requestAnimationFrame(render)
   }
--- src/vs1.vert
+++ src/vs1.vert
@@ -1,7 +1,15 @@
 #version 300 es
 
 in vec4 position;
+in vec4 color;
+
+uniform mat4 cam_proj;
+uniform mat4 cam_pos_inv;
+uniform mat4 obj_pos;
+
+out vec4 v_color;
 
 void main() {
-  gl_Position = position;
+  gl_Position = cam_proj * cam_pos_inv * obj_pos * position;
+  v_color = color;
 }
